67c4249b3e750027a4036a670626fc3711a366f3
simri77 <simretmesfin97@gmail.com>
2023-01-06 12:24:41 -0800

    function  returns a map of groceries with the following items

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const mapObject = new Map();
+  return mapObject
+    .set('Apples', 10)
+    .set('Tomatoes', 10)
+    .set('Pasta', 1)
+    .set('Rice', 1)
+    .set('Banana', 5);
+}
